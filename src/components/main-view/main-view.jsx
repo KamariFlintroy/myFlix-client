11bc3053150f1caa245f6eff9e698022733e376a
KamariFlintroy <kamariflintroy@gmail.com>
2024-09-18 23:58:51 -0400

    Added main-view, movie-card, movie-view

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { MovieCard } from '../movie-card/movie-card.jsx';
+import { MovieView } from '../movie-view/movie-view.jsx';
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: 'The Avengers',
+      genre: 'Action',
+      director: 'Joss Whedon',
+    },
+    {
+      id: 2,
+      title: 'Avengers: Age of Ultron',
+      genre: 'Action',
+      director: 'Joss Whedon',
+    },
+    {
+      id: 3,
+      title: 'Avengers: Infinity War',
+      genre: 'Action',
+      director: 'Anthony Russo',
+    },
+    {
+      id: 4,
+      title: 'Avengers: Endgame',
+      genre: 'Action',
+      director: 'Anthony Russo',
+    },
+    {
+      id: 5,
+      title: 'Avengers: Secret Wars',
+      genre: 'Action',
+      director: 'Anthony Russo',
+    },
+  ]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  if (selectedMovie) {
+    return (
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
+    );
+  }
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  } else {
+    return (
+      <div>
+
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ))}
+
+      </div>
+    );
+  }
+};
